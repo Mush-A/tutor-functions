8fe9b944acfb11e0b2e2b2dc0955ce0f9e8524bf
Mush-A <mushrafaltaf@gmail.com>
2024-03-06 09:59:45 +0530

    Add emulators configuration and logging to functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as admin from "firebase-admin";
 import * as nodemailer from "nodemailer";
 import { transporter } from "./transporter";
 import { bookingTemplate } from "./email-templates/booking-template";
+import * as logger from "firebase-functions/logger";
 
 admin.initializeApp();
 
@@ -25,8 +26,47 @@ export const sendEmail = functions.firestore
 
 		try {
 			await transporter.sendMail(mailOptions);
-			console.log("Booking email sent successfully");
+			logger.log("Booking email sent successfully");
 		} catch (error) {
-			console.error("Error sending welcome email:", error);
+			logger.error("Error sending welcome email:", error);
 		}
 	});
+
+export const assignStudentRole = functions.firestore
+	.document("students/{studentId}")
+	.onCreate(async (snapshot, context) => {
+		try {
+			const student = snapshot.data();
+			logger.log("Student:", student);
+			const user = await admin.auth().getUser(context.params.studentId);
+			logger.log("User:", user);
+			await admin.auth().setCustomUserClaims(user.uid, {
+				student: true,
+			});
+			logger.log("Student role assigned to:", user.email);
+		} catch (e) {
+			logger.error("Error assigning student role:", e);
+		}
+	});
+
+export const addTutorRole = functions.https.onCall(async (data, context) => {
+	try {
+		const email = data.email;
+		const user = await admin.auth().getUserByEmail(email);
+		if (user.customClaims && (user.customClaims as any).tutor === true) {
+			return;
+		}
+		await admin.auth().setCustomUserClaims(user.uid, {
+			tutor: true,
+		});
+
+		return {
+			message: `Success! ${email} has been made a tutor.`,
+		};
+	} catch (e) {
+		logger.log("[addTutorRole] Something went wrong", e);
+		return {
+			message: `Error! ${e}. User could not be made a tutor.`,
+		};
+	}
+});
